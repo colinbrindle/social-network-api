93f345bb40deca40c9726a302a9e214375753609
colin brindle <hcolinbrindle@gmail.com>
2022-05-28 17:38:57 -0400

    Created get and post requests for users

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const { ObjectId } = require("mongoose").Types;
+const { User } = require("../models");
+
+/* User APIs
+
+GET all users
+
+GET a single user by its _id and populated thought and friend data
+
+POST a new user:
+
+// example data
+{
+  "username": "lernantino",
+  "email": "lernantino@gmail.com"
+}
+PUT to update a user by its _id
+
+DELETE to remove user by its _id
+
+*/
+
+// GET all users
+const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    res.status(200).json(allUsers);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+// POST new user
+const addNewUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(200).json(newUser);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  addNewUser,
+};
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,6 @@
 const router = require("express").Router();
+const { getAllUsers, addNewUser } = require("../../controllers/userController");
+
+router.route("/").get(getAllUsers).post(addNewUser);
 
 module.exports = router;
